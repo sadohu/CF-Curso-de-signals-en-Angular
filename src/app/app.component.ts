e5aa051530f1177acd94812845e52dca85a34378
sadohu <sadohu26@yahoo.com>
2024-01-09 12:26:06 -0500

    Add task functionality and mark task as completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+interface Task {
+  name: string;
+  isCompleted: boolean;
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -8,6 +13,7 @@ import { CommonModule } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'signals-example';
 
@@ -19,8 +25,25 @@ export class AppComponent {
   // name = signal('Hugo'); -> String
 
   name = signal<any>('Hugo');
+  tasks = signal<Task[]>([
+    { name: 'Watch signals course', isCompleted: false },
+  ]);
 
   toggleName() {
     this.name.set('Donie');
   }
+
+  addRandomTask() {
+    this.tasks.update(tasks => {
+      return [...tasks, { name: 'Practice everyday', isCompleted: false }];
+    });
+  }
+
+  markTaskAsCompleted(task: Task) {
+    this.tasks.mutate(tasks => {
+      const taskIndex = tasks.indexOf(task);
+      tasks[taskIndex].isCompleted = true;
+    });
+  }
+
 }
