b0bf836e37ad2c7a741200acc02b8c957eb9689b
sadohu <sadohu26@yahoo.com>
2024-01-09 11:17:49 -0500

    Add signal import and update name signal in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -10,4 +10,17 @@ import { CommonModule } from '@angular/common';
 })
 export class AppComponent {
   title = 'signals-example';
+
+  // Create a signal with a default value
+
+  // Between the <> you can specify the type of the signal
+  // If you don't specify a type,
+  // the type will be automatically inferred from the default value
+  // name = signal('Hugo'); -> String
+
+  name = signal<any>('Hugo');
+
+  toggleName() {
+    this.name.set('Donie');
+  }
 }
