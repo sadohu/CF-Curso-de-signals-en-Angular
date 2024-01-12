3539ac18c21c22137790e9a1736123d39ddb49b7
sadohu <sadohu26@yahoo.com>
2024-01-11 23:53:41 -0500

    [Refator] All Project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'signals-example' title`, () => {
+  it(`should have the 'angular-signals' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('signals-example');
+    expect(app.title).toEqual('angular-signals');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('signals-example app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-signals app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,89 +1,15 @@
-import { Component, OnInit, computed, effect, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { toObservable } from '@angular/core/rxjs-interop';
-import { map } from 'rxjs';
-
-interface Task {
-  name: string;
-  isCompleted: boolean;
-}
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
-
-export class AppComponent implements OnInit {
-  title = 'signals-example';
-
-  /* Signals and Variables */
-
-  // Create a signal with a default value
-
-  // Between the <> you can specify the type of the signal
-  // If you don't specify a type,
-  // the type will be automatically inferred from the default value
-  // name = signal('Hugo'); -> String
-
-  name = signal<any>('Hugo');
-  tasks = signal<Task[]>([
-    { name: 'Watch signals course', isCompleted: false },
-  ]);
-
-  // asReadonly() returns a readonly version of the signal
-  description = signal('Signals are awesome!').asReadonly();
-
-  // Computed signals are signals that are computed from other signals
-  // and they are stored in memory
-  taskLength = computed(() => this.tasks().length);
-
-  // toObservable() returns an observable version of the signal
-  // This is useful when you want to use the async pipe in the template
-  tasks$ = toObservable(this.tasks);
-
-  constructor() {
-    // Effects are signals that are computed from other signals
-    // but they don't have a value and they are not stored in memory
-    effect(() => {
-      if (this.taskLength() > 5)
-        alert(`You have a lot of tasks to do!`);
-
-    });
-  }
-
-  ngOnInit(): void {
-    // this.tasks$.subscribe(response => {
-    //   console.log("response", response);
-    // });
-
-    this.tasks$.pipe(
-      map(result => {
-        const newTaks = result.map(task => ({ ...task, creationate: new Date() }));
-        return newTaks;
-      })
-    ).subscribe(response => console.log("response", response));
-  }
-
-  /* Methods */
-
-  toggleName() {
-    this.name.set('Donie');
-  }
-
-  addRandomTask() {
-    this.tasks.update(tasks => {
-      return [...tasks, { name: 'Practice everyday', isCompleted: false }];
-    });
-  }
-
-  markTaskAsCompleted(task: Task) {
-    this.tasks.mutate(tasks => {
-      const taskIndex = tasks.indexOf(task);
-      tasks[taskIndex].isCompleted = true;
-    });
-  }
-
+export class AppComponent {
+  title = 'angular-signals';
 }
